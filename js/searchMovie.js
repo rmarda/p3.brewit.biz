6a349e49f2bb28cb79845042ef4d86bc5b718a81
Ruchika Marda <rmarda4@gmail.com>
2013-12-01 15:48:04 -0500

    images for search movie

--- js/searchMovie.js
+++ js/searchMovie.js
@@ -21,17 +21,28 @@ $(function() {
 
 function parseResults(data) {
 
-    var results = JSON.stringify(data);
-    $('#search_results .inner').html('Displaying results' + results);
+    var base_url = window.configData.images['base_url'];
+    var image_size = window.configData.images['poster_sizes'][1];
+    var url = base_url + image_size;
+    //$('#search_results .inner').html('Displaying results' + results);
+    $('#search_results .inner ').css('list-style-type', 'none');
+    $('#search_results .inner').append('<ul>');
+    for(var i = 0; i<data.results.length; i++) {
+        var movie_name = data.results[i].original_title;
+        var image_path = data.results[i].poster_path;
+        var release_date = data.results[i].release_date;
+        var vote_average = data.results[i].vote_average;
+        var vote_count = data.results[i].vote_count;
 
-    /*var titles = '';
-    for(var i = 0; i <data.results.length; i++)
-        titles = titles + ' ' + data.results[i].original_title;
-
-    alert(titles);*/
-
-    //var myobj = {name:"Ruchika", age: 32};
-    //var abc = $.parseJSON(myobj);
-    //alert(JSON.stringify(myobj));
+        $('#search_results .inner').append('<li><img src="' + url + image_path + '" /> </li>');
+        $('#search_results .inner').append(movie_name);
+        $('#search_results .inner').append('<br>');
+        $('#search_results .inner').append('Release Date: '+release_date);
+        $('#search_results .inner').append('<br>');
+        $('#search_results .inner').append('Avg. Rating: '+ vote_average);
+        $('#search_results .inner').append('<br>');
+        $('#search_results .inner').append('Total no. of votes: '+vote_count);
+    }
+    $('#search_results .inner').append('</ul>');
 
 }
