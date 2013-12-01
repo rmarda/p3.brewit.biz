93099a2ed206909b0b111ffcb5b7378b8f8ca47a
Ruchika Marda <rmarda4@gmail.com>
2013-12-01 16:42:00 -0500

    error checking for invalid movie names or zero results.

--- js/upcoming.js
+++ js/upcoming.js
@@ -15,20 +15,33 @@ $(function() {
 
 function parseResults(data) {
 
+    if(data.results.length == 0){
+        $('#search_results .inner').html('We didn\'t find any movies that matched your query.');
+        return;
+    }
+
     var base_url = window.configData.images['base_url'];
     var image_size = window.configData.images['poster_sizes'][1];
     var url = base_url + image_size;
-    //$('#search_results .inner').html('Displaying results' + results);
+    // clear out the page
+    $('#search_results .inner').empty();
+
     $('#search_results .inner ').css('list-style-type', 'none');
     $('#search_results .inner').append('<ul>');
     for(var i = 0; i<data.results.length; i++) {
+
+        var image = data.results[i].poster_path;
+        var image_path = '';
+        if(image == null)
+            continue;
+        else
+            image_path = url + data.results[i].poster_path;
         var movie_name = data.results[i].original_title;
-        var image_path = data.results[i].poster_path;
         var release_date = data.results[i].release_date;
         var vote_average = data.results[i].vote_average;
         var vote_count = data.results[i].vote_count;
 
-        $('#search_results .inner').append('<li><img src="' + url + image_path + '" /> </li>');
+        $('#search_results .inner').append('<li><img src="' + image_path + '" /> </li>');
         $('#search_results .inner').append(movie_name);
         $('#search_results .inner').append('<br>');
         $('#search_results .inner').append('Release Date: '+release_date);
@@ -38,4 +51,6 @@ function parseResults(data) {
         $('#search_results .inner').append('Total no. of votes: '+vote_count);
     }
     $('#search_results .inner').append('</ul>');
+
+
 }
