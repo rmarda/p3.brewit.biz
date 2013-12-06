ff93f2cd440fa8f8ef87597872244f24751f5c80
Ruchika Marda <rmarda4@gmail.com>
2013-12-06 18:57:36 -0500

    minor index page formatting updates

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
             </div>
         </section>
         <header>
-            <a href='index.php'><img id ='logo' src='images/movie_monkey.jpg' alt='movie_monkey_logo' width='125' height='125'/></a>
+            <a href="index.php"><img id ='logo' src='images/movie_monkey.jpg' alt='movie_monkey_logo' width='125' height='125'/></a>
             <h1 id='page_title'>Movie Monkey</h1>
         </header>
         <nav class='menu'>
@@ -33,28 +33,28 @@
             </ul>
         </nav>
         <section id='feature_area'>
+            <article id="home_page_text">
 
-            <h2>Welcome user!</h2>
-            <p>
-                This website was prepared as a course work for <a href="http://www.extension.harvard.edu/courses/dynamic-web-applications">CSCI E-15 Dynamic Web Applications (Fall 2013)</a><br>
-            </p>
-            <p>
-                You can navigate through the website using any of the menu items in the menu bar<br>
-                <strong>Home:</strong> Will bring you right here!<br>
-                <strong>Searh:</strong> You can lookup movie names using a keyword in the search box.<br>
-                <strong>Now Playing:</strong> Lists movies currently playing in theaters.<br>
-                <strong>Searh:</strong> Lists movies that are releasing soon.<br>
-            </p>
+                <h2>Welcome user!</h2>
+                <p>
+                    This website has been prepared as a course work for <a href="http://www.extension.harvard.edu/courses/dynamic-web-applications">CSCI E-15 Dynamic Web Applications (Fall 2013)</a><br>
+                </p>
+                <p>
+                    You can navigate through the website using any of the menu items in the menu bar:<br>
+                    <strong class = "menu_item">Home:</strong> Will bring you right here!<br>
+                    <strong class = "menu_item">Searh Movie:</strong> You can lookup movie names using a keyword in the search box.<br>
+                    <strong class = "menu_item">Now Playing:</strong> Lists movies currently playing in theaters.<br>
+                    <strong class = "menu_item">Upcoming:</strong> Lists movies that are releasing soon.<br>
+                </p>
 
-            <p>
-                The information displayed is being fetched by a third-party site: <a href="http://www.themoviedb.org/">The Movie Database.</a><br>
-            </p>
-            <p>
-                <strong>Limitations/Disclaimers:</strong>
-                Movie Database website enforces the following rules on the number of searches that can be made on their website:<br>
-                30 requests every 10 seconds per IP. Maximum 20 simultaneous connections.
-                For more information, please visit <a href="http://docs.themoviedb.apiary.io/">here:</a>
-            </p>
+                <p>
+                    The information displayed is being fetched from a third-party site: <a href="http://www.themoviedb.org/">The Movie Database.</a><br>
+                    <strong class="limitations">Limitations/Disclaimers:</strong>
+                    Movie Database enforces the following rules on the search limit:<br>
+                    30 requests every 10 seconds per IP. Maximum 20 simultaneous connections.<br>
+                    For more information, please visit their website <a href="http://docs.themoviedb.apiary.io/">here.</a>
+                </p>
+             </article>
         </section>
         <footer>
             <p>Movie API from <a href="https://www.themoviedb.org/"> MovieDB.</a> Logo taken from <a href="http://www.logoinstant.com/"> LogoInstant</a></p>
